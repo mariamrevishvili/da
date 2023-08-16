4d0d5a8cb4bd7762afadf5fb4d1a4f5ca6bb8522
mariamrevishvili <140517621+mariamrevishvili@users.noreply.github.com>
2023-08-16 22:04:59 +0400

    Add files via upload

---
+++ davaleba7.py
@@ -0,0 +1,13 @@
+questions = "You will have small test in math try to get all questions correct: "
+
+math_1 = int(input("8 * 5 = "))
+
+math_2 = int(input("549 + 381 = "))
+
+math_3 = int(input("120 / 5 = "))
+
+math_4 = int(input("1678 - 534 = "))
+
+answers = (math_1 == 64) and (math_2 == 930) and (math_3 == 24) and (math_4 == 1144)
+
+print(answers)
